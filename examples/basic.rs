6f43a0cdcdaeaf2f2437d828e8d052473134d188
JulianBohne <julianbohnenkaemper@gmail.com>
2024-09-06 22:40:14 +0200

    Fixed basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,7 +1,10 @@
 use markov_text::MarkovTextModel;
 
 fn main() {
-    let sample_texts = vec!["Math is dumb", "Timo is cool"];
+    let sample_texts = ["Math is dumb", "Timo is cool"]
+        .iter()
+        .map(|&s| s.to_string())
+        .collect();
 
     let mut model = MarkovTextModel::<4>::new();
     model.add_sample_texts(&sample_texts);
